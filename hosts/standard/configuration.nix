1d899b65e44793f6c2b83df2cda07ac87a70faaf
Ankit Prajapati <ap8424856@gmail.com>
2025-11-08 15:14:45 +0530

    boot fix

--- hosts/standard/configuration.nix
+++ hosts/standard/configuration.nix
@@ -1,12 +1,16 @@
 { config, pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
+    ../../modules/boot.nix
     ../../modules/networks.nix
     ../../modules/users.nix
   ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   environment.systemPackages = with pkgs; [
   ];
   home-manager.users.ankit = import ./home.nix;
   
+  system.stateVersion = "25.05";
 }
