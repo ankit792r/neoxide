002afd27b637dcb030b03b6b3f15b8110e92f0af
Ankit Prajapati <ap8424856@gmail.com>
2025-11-08 15:03:30 +0530

    username fix 3

--- hosts/standard/configuration.nix
+++ hosts/standard/configuration.nix
@@ -5,17 +5,18 @@
     ../../modules/users.nix
   ];
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  networking.networkmanager.enable = true;
-  
-  environment.systemPackages = with pkgs; [
-  ];
-
   let
     usernames = builtins.attrNames config.users.users;
     username = builtins.head usernames;
   in {
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
+    networking.networkmanager.enable = true;
+  
+    environment.systemPackages = with pkgs; [
+    ];
+
+  
     home-manager.users."${username}" = import ./home.nix;
   };
 }
