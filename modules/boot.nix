3ebda47b3d3f28037fbec57904d78da3b443f8ae
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2025-11-12 11:19:29 +0000

    updated boot

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,5 +1,15 @@
+## this boot config is for VM machine 
+
 { config, ...}: 
 {
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
+
+    boot.initrd.kernelModules = [ "hv_vmbus" "hv_storvsc" "hv_netvsc" "hv_utils" ];
+    boot.kernelModules = [ "hv_vmbus" "hv_storvsc" "hv_netvsc" "hv_utils" ];
+
+    systemd.services.hv-kvp = {
+    after = [ "sys-devices-virtual-vmbus-hv_kvp.device" "vmbus.service" ];
+    wants = [ "sys-devices-virtual-vmbus-hv_kvp.device" ];
+  };
 }
