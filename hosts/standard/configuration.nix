b73bb1e37c88fac15c5a233d311c88e52f361541
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2025-11-12 10:28:50 +0000

    added var in minimal

--- hosts/standard/configuration.nix
+++ hosts/standard/configuration.nix
@@ -4,14 +4,15 @@
     ../../modules/boot.nix
     ../../modules/networks.nix
     ../../modules/users.nix
-     ./variables.nix
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   environment.systemPackages = with pkgs; [];
 
-  home-manager.users."${config.var.username}" = import ./home.nix;
+  home-manager.users.ankit = import ./home.nix;
   
   system.stateVersion = "25.05";
 }
+
+# this is nix config proect with flakes, i have two flake minimal and standard, using both in main @flake.nix  file. but while testing this config getting error as attribute username is missing  in @configuration.nix file, why getting error and how to fix that
