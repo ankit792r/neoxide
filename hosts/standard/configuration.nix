6ee6b61e390e629036c2eb7e6d3a2493e6a09053
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2025-11-12 10:30:54 +0000

    added fixed username in var issue

--- hosts/standard/configuration.nix
+++ hosts/standard/configuration.nix
@@ -4,13 +4,14 @@
     ../../modules/boot.nix
     ../../modules/networks.nix
     ../../modules/users.nix
+    ./variables.nix
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   environment.systemPackages = with pkgs; [];
 
-  home-manager.users.ankit = import ./home.nix;
+  home-manager.users."${config.var.username}" = import ./home.nix;
   
   system.stateVersion = "25.05";
 }
